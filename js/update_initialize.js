3f636e6aa57cf8d74b4876550e7189f730c2fdb3
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2022-12-15 12:59:37 +0900

    Update update_initialize.js

--- js/update_initialize.js
+++ js/update_initialize.js
@@ -51,12 +51,12 @@ $(function(){
 		let message = '';
 		
 		if(newName!==''){
-			message = '[変更後の児童名]\n' + newName + '\n';
+			message = '[変更後の児童名]\n' + newName;
 		}
-		if(newOffice==='smileday') message = message + '[変更後の事業所]\nスマイル\n';
-		else if(newOffice==='temu') message = message + '[変更後の事業所]\nてむてむ\n';
-		if(newOffice==='hoya') message = message + '[変更後の事業所]\nほやほや\n';
-		if(newOffice==='naru') message = message + '[変更後の事業所]\nなるなる\n';
+		if(newOffice==='smileday') message = message + '\n[変更後の事業所]\nスマイル\n';
+		else if(newOffice==='temu') message = message + '\n[変更後の事業所]\nてむてむ\n';
+		if(newOffice==='hoya') message = message + '\n[変更後の事業所]\nほやほや\n';
+		if(newOffice==='naru') message = message + '\n[変更後の事業所]\nなるなる\n';
 		
 		sendText(message);
 		return false;
