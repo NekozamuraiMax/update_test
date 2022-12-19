04ab6a14edee8e08aa0f576f251d4b68e97f319c
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2022-12-19 21:31:55 +0900

    Update update_initialize.js

--- js/update_initialize.js
+++ js/update_initialize.js
@@ -1,4 +1,7 @@
-const id = "1657662321-pL1Lqjzn";
+const url = new URL(document.location);
+const params = new URLSearchParams(url.search);
+const id = params.get('id');
+
 window.onload = function(e){
 	liff.init({
 		liffId: id
