04ff83079a4cd8bca8938a629f7a8cbb16bf7624
NekozamuraiMax <118565220+NekozamuraiMax@users.noreply.github.com>
2023-01-31 12:01:34 +0900

    Add files via upload

---
+++ js/update_form.js
@@ -0,0 +1,29 @@
+document.getElementById('delete1').addEventListener('click', () => {
+	child1_update.style.display = 'none';
+	delete_msg1.style.display = 'block';
+});
+
+document.getElementById('back1').addEventListener('click', () => {
+	child1_update.style.display = 'block';
+	delete_msg1.style.display = 'none';
+});
+
+document.getElementById('delete2').addEventListener('click', () => {
+	child2_update.style.display = 'none';
+	delete_msg2.style.display = 'blodk';
+});
+
+document.getElementById('back2').addEventListener('click', () => {
+	child2_update.style.display = 'block';
+	delete_msg2.style.display = 'none';
+});
+
+document.getElementById('delete3').addEventListener('click', () => {
+	child3_update.style.display = 'none';
+	delete_msg3.style.display = 'blodk';
+});
+
+document.getElementById('back3').addEventListener('click', () => {
+	child3_update.style.display = 'block';
+	delete_msg3.style.display = 'none';
+});
